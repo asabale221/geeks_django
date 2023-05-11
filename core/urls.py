e68c3d26d707cbfc05f1da395b4d18a8b192ae39
asabale221 <119834965+asabale221@users.noreply.github.com>
2023-05-11 18:49:19 +0600

    1212 git commit -m  1212

--- core/urls.py
+++ core/urls.py
@@ -17,13 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
-from django.conf import settings
+from django.conf import setting
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("setting.urls"))
 ]
-urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+urlpatterns += static(setting.MEDIA_URL,document_root = setting.MEDIA_ROOT)
 
 
 
